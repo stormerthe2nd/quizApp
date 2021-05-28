bd48d9ea8d8ba173fd43f1f4520d3d492092d800
Junaid Firdosi <theiconsting17@gmail.com>
2021-05-29 05:22:13 +0530

    carausal added to index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-    $(".quiz-objects").mouseenter(function () {
-        $(this).css({ "animation-name": "example", "background-color": "rgb(126, 43, 146)" })
 
+$(document).ready(function () {
+    $(".quiz-box").mouseenter(function () {
+        $(this).css({ "animation-name": "example", "background-color": "rgb(126, 43, 146)" })
     }).mouseleave(function () {
-        $(this).css({ "animation-name": "none", "background-color": "firebrick" })
+        $(this).css({ "animation-name": "none", "background-color": "blueviolet" })
     })
 });
